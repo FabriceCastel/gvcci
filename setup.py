14c68f43604aaad5b34aab7f1a192714ea1f5d91
Fabrice Castel <fabrice.castel@hotmail.com>
2017-10-20 17:07:52 -0400

    numpy and scikit dependencies should be included in the setup script

--- setup.py
+++ setup.py
@@ -3,6 +3,11 @@ from setuptools import setup, find_packages
 setup(
     name='colorizer-terminator',
     packages=find_packages(),
-    install_requires=["hasel>=1.0.1"],
+    install_requires=[
+        "numpy",
+        "scikit-learn",
+        "scikit-image",
+        "hasel>=1.0.1"
+    ],
     dependency_links = ["https://github.com/sumartoyo/hasel.git/tarball/master#egg=package-1.0.1"]
 )
