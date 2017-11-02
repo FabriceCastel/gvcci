38bc35bf0973cc1532ae57f08b58a0cbf0a2b09a
Fabrice Castel <fabrice.castel@hotmail.com>
2017-11-01 22:22:02 -0400

    split out iterm utils for convenience

--- iterm_lazy.sh
+++ iterm_lazy.sh
@@ -1,20 +1,5 @@
 #!/bin/bash
 
 python3 extract.py "$1" --template templates/iterm.plist --background dark
-cp ~/.gvcci/themes/$(basename $1 | cut -d'.' -f1)/iterm.plist ~/Library/Application\ Support/iTerm2/DynamicProfiles/gvcci.plist
-
-# set macOS wallpaper
-img_path="$(cd "$(dirname "$1")"; pwd)/$(basename "$1")"
-cmd="tell application \"Finder\" to set desktop picture to POSIX file \"$img_path\""
-echo "$cmd"
-osascript -e "$cmd"
-
-# This doesn't matter if the gvcci profile is set as default
-sleep 3 # wait for iTerm to pick up the new profile before trying to load it
-echo -e "\033]50;SetProfile=gvcci\a"
-
-echo "If you want your iTerm color scheme to update automatically every"
-echo "time you run this script, go to:"
-echo "Preferences > Profiles > gvcci"
-echo "Then under 'Other Actions...' select 'Set as Default'"
-echo "This will take effect for all new terminal sessions"
+img_name="$(basename "$1" | cut -d'.' -f1)"
+./iterm_apply.sh $img_name
