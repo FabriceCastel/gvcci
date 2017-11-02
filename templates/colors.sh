bd5f9a47b5c0d9b9d20fed08b31f40285fc565ac
Ariela Wenner <sunarisunz@gmail.com>
2017-11-02 01:27:00 -0300

    Added xresources and shell script templates

---
+++ templates/colors.sh
@@ -0,0 +1,23 @@
+
+# Special
+background='{{background-hex}}'
+foreground='{{foreground-hex}}'
+cursor='{{foreground-hex}}'
+
+# Colors
+color0='{{ansi-black-normal-hex}}'
+color1='{{ansi-red-normal-hex}}'
+color2='{{ansi-green-normal-hex}}'
+color3='{{ansi-yellow-normal-hex}}'
+color4='{{ansi-blue-normal-hex}}'
+color5='{{ansi-magenta-normal-hex}}'
+color6='{{ansi-cyan-normal-hex}}'
+color7='{{ansi-white-normal-hex}}'
+color8='{{ansi-black-bright-hex}}'
+color9='{{ansi-red-bright-hex}}'
+color10='{{ansi-green-bright-hex}}'
+color11='{{ansi-yellow-bright-hex}}'
+color12='{{ansi-blue-bright-hex}}'
+color13='{{ansi-magenta-bright-hex}}'
+color14='{{ansi-cyan-bright-hex}}'
+color15='{{ansi-white-bright-hex}}'
