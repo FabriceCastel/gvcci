6cbbd6cb2ffa81bf609cb8a755faa84bd6361add
Fabrice Castel <fabrice.castel@hotmail.com>
2017-11-04 16:42:13 -0400

    the apply script should be given the correct params

--- iterm_lazy.sh
+++ iterm_lazy.sh
@@ -2,4 +2,4 @@
 
 python3 extract.py "$1" --template templates/iterm.plist --background dark
 img_name="$(basename "$1" | cut -d'.' -f1)"
-./gvcci-apply/iterm_apply.sh $img_name
+./gvcci-apply/apply.sh $img_name iterm
