7bcf9faec697d070e82a5bb58c0333769de3e809
Fabrice Castel <fabrice.castel@hotmail.com>
2017-11-09 17:11:53 -0500

    remove useless Argument class, it's a net negative

--- config.py
+++ config.py
@@ -3,70 +3,38 @@ import os
 import sys
 import json
 
-class Argument:
-	def __init__(self, name, help, dest, default=None):
-		self.name = name
-		self.help = help
-		self.default = default
-		self.dest = dest
-
-	def add_to_parser(self, parser):
-
-		# the first param for a positional argument is considered to be the dest so you
-		# don't need to pass it in as a named param
-		if (self.name[:2] != "--"):
-			parser.add_argument(
-				self.name,
-				default=self.default,
-				help=self.help,
-				nargs="+"
-			)
-		elif (self.default == None):
-			parser.add_argument(
-				self.name,
-				dest=self.dest,
-				help=self.help,
-				type=str
-			)
-		else:
-			parser.add_argument(
-				self.name,
-				default=self.default,
-				dest=self.dest,
-				help=self.help,
-				type=str
-			)
-
-arguments = [
-	Argument(
-		name="images",
-		help="The images to generate color themes for.",
-		dest="images"
-	),
-	Argument(
-		name="--background",
-		help="[dark|light|<#hex_code>] specify whether the script should generate a light/dark theme or use a specific hex color for the background.",
-		default="auto",
-		dest="background"
-	),
-	Argument(
-		name="--template",
-		help="Template or directory of templates to use for the theme.",
-		default="./templates/",
-		dest="template_path"
-	),
-	Argument(
-		name="--config",
-		help="Config file to use. Config settings will override other commandline params.",
-		dest="config_path"
-	)
-]
 
 def get_args():
 	parser = argparse.ArgumentParser(description="Create terminal themes to match wallpaper images.")
 
-	for argument in arguments:
-		argument.add_to_parser(parser)
+	parser.add_argument(
+		"images",
+		help="The images to generate color themes for.",
+		nargs="+"
+	)
+
+	parser.add_argument(
+		"--background",
+		help="[dark|light|<#hex_code>] specify whether the script should generate a light/dark theme or use a specific hex color for the background.",
+		default="auto",
+		dest="background",
+		type=str
+	)
+
+	parser.add_argument(
+		"--template",
+		help="Template or directory of templates to use for the theme.",
+		default="./templates/",
+		dest="template_path",
+		type=str
+	)
+
+	parser.add_argument(
+		"--config",
+		help="Config file to use. Config settings will override other commandline params.",
+		dest="config_path",
+		type=str
+	)
 
 	args = vars(parser.parse_args())
 	config_path = args['config_path']
